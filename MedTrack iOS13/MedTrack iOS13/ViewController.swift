033b716ab318fedc5df35ef632b3986157f2ed93
Kyle Nguyen <58537629+kylenguyen-cs30@users.noreply.github.com>
2023-01-08 16:51:33 -0800

    upload initial files

---
+++ MedTrack iOS13/MedTrack iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedTrack iOS13
+//
+//  Created by Kyle Nguyen on 1/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
