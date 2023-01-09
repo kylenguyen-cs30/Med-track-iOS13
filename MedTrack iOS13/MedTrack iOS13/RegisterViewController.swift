81e9a7836b06ec7f763d58c4d03a65750c3d653d
Kyle Nguyen <58537629+kylenguyen-cs30@users.noreply.github.com>
2023-01-08 17:23:32 -0800

    Second Update

---
+++ MedTrack iOS13/MedTrack iOS13/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterViewController.swift
+//  MedTrack iOS13
+//
+//  Created by Kyle Nguyen on 1/8/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
